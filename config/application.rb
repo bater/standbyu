cca1f9a253dbd6a61567a1113b272b2e6ece4039
bater <jace_ka@yahoo.com.tw>
2017-05-19 21:50:02 +1000

    fix heroku vender bug

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Standbyu
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
+    config.assets.precompile += %w(vendor/assets/images/*)
   end
 end
