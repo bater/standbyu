34083b67080cb3320d67468db0778b8226ed0c30
bater <jace_ka@yahoo.com.tw>
2017-05-19 15:00:07 +1000

    change posts route to backend

--- app/views/posts/_post.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! post, :id, :title, :city, :district, :adress, :phone, :service_time, :tag, :description, :url, :created_at, :updated_at
-json.url post_url(post, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  namespace :backend do
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount TaiwanCity::Engine => '/taiwan_city'
 end
